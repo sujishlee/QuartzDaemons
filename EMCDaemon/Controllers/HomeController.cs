0c10ad4552784e6c1d4ae28ff8c344e8c642f031
Sujish Lee <sujish_lee@Dell.com>
2020-01-08 11:43:09 +0530

    Created New Schedulers

---
+++ EMCDaemon/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EMCDaemon.Models;
+using Quartz;
+using Dell.DP.EMC.ReportDataProcessor.Jobs;
+
+namespace EMCDaemon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StartEMCReportDataProcessor()
+        {
+            IJobDetail job = JobBuilder.Create<EMCReportDataProcessor>()
+                            .WithIdentity("EMCReportDataProcessor", "ReportDataProcessor")
+                            .Build();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EMCDaemon/Jobs/EMCReportDataProcessor.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dell.DP.EMC.ReportDataProcessor.Jobs
+{
+    public class EMCReportDataProcessor : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Debug.WriteLine("Job started");
+        }
+    }
+}
